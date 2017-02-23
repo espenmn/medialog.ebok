6016c15b661edfe3a8a3d06c6a81365903e541e5
Espen Moe-Nilssen <espen@medialog.no>
2017-02-23 17:59:50 +0100

    add transformations

--- src/medialog/ebok/browser/book.py
+++ src/medialog/ebok/browser/book.py
@@ -5,11 +5,21 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class BookView(BrowserView):
     """ Converter view for Bok.
     """
+    
+    transformations = (
+        'makeImagesLocal',
+        'convertFootnotes',
+        'removeCrapFromHeadings',
+        'fixHierarchies',
+        'addTableOfContents',
+    )
 
     template = ViewPageTemplateFile('book.pt')
 
     def __call__(self, *args, **kw):
         return self.template(self.context)
+        
+    
 
 InitializeClass(BookView)
 
